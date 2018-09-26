2562e6b0b4222a05297f35d40f43d5d4dc9e45f3
Mohammed Dawood <mahmedtony73@gmail.com>
2018-09-26 04:38:30 +0200

    Feat: Adding a review component

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//product component
+
+
 Vue.component('product',{
     props: {
         premium: {
@@ -6,7 +9,7 @@ Vue.component('product',{
         }
     },
     template:`
-    <div class="product">
+<div class="product">
 
     <div class="product-image">
         <img v-bind:src="image">
@@ -31,15 +34,24 @@ Vue.component('product',{
             :style='{backgroundColor:variant.variantColor }'
             @mouseover="updateProduct(index)">
     </div>
-
-    <button v-on:click="addToCart();"
-    :disabled="!inStock"
-    :class="{disabledButton: !inStock}">Add to cart</button>
-
     <div>
-        <p>Cart({{cart}})</p>
+        <button v-on:click="addToCart();"
+        :disabled="!inStock"
+        :class="{disabledButton: !inStock}">Add to cart</button>
+    </div>
+    <div>
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">There are no reviews yet</p>
+        <ul>
+            <li v-for="review in reviews">
+                <p>{{ review.name }}</p>
+                <p>Rating:{{ review.rating }}</p>
+                <p>{{ review.review }}</p>
+            </li>
+        </ul>
     </div>
 
+    <product-review @review-submitted="addReview"></product-review>
 </div>`,
 data() {
     return {
@@ -61,17 +73,20 @@ data() {
                 variantQuantity:0
             }
         ],
-        cart:0
+        reviews: []
     }
 },
 methods:{
     addToCart: function() {
-        this.cart += 1
+        this.$emit('add-to-cart',this.variants[this.selectedVariant].variantId)
         this.variants[this.selectedVariant].variantQuantity -= 1
     },
     updateProduct: function(index) {
         this.selectedVariant = index
         console.log(index)
+    },
+    addReview: function(productReview) {
+        this.reviews.push(productReview)
     }
 },
 computed: {
@@ -94,9 +109,83 @@ computed: {
 }
 })
 
+//review component
+Vue.component('product-review',{
+    template:`    <form class="review-form" @submit.prevent="onSubmit">
+    <P v-if="errors.length">
+        <b>Please correct the following error(s)</b>
+        <ul>
+            <li v-for="error in errors">{{ error }}</li>
+        </ul>
+    </p>
+    
+    <p>
+      <label for="name">Name:</label>
+      <input id="name" v-model="name" placeholder="name">
+    </p>
+
+    <p>
+      <label for="review">Review:</label>
+      <textarea id="review" v-model="review"></textarea>
+    </p>
+
+    <p>
+      <label for="rating">Rating:</label>
+      <select id="rating" v-model.number="rating">
+        <option>5</option>
+        <option>4</option>
+        <option>3</option>
+        <option>2</option>
+        <option>1</option>
+      </select>
+    </p>
+
+    <p>
+      <input type="submit" value="Submit">
+    </p>
+
+  </form>`,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors:[]
+        }
+    },
+    methods: {
+        onSubmit () {
+            if(this.name && this.review && this.rating){
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                    }
+                    this.$emit('review-submitted', productReview)
+                    this.name = null
+                    this.review = null
+                    this.rating = null
+                    this.errors = []
+            }
+            else{
+                this.errors = []
+                if(!this.name) this.errors.push("name required")
+                if(!this.review) this.errors.push("review required")
+                if(!this.rating) this.errors.push("rating required")   
+            }
+        }
+    }
+})
+//root
 var app = new Vue({
     el: '#app',
     data: {
-        premium: true
+        premium: true,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        }
     }
 })
